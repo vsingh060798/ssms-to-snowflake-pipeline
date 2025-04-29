cc57c1f250305738781962a6e87b27f519ed9f67
Vishwas Singh <vishwas.singh@ascendion.com>
2025-04-29 12:09:41 +0530

    Initial project commit - SSMS to Snowflake Pipeline

---
+++ SQL Query/SQLQuery1.sql
@@ -0,0 +1,16 @@
+-- Insert into players
+INSERT INTO players VALUES
+(1, 'Virat Kohli', 'India'),
+(2, 'Steve Smith', 'Australia'),
+(3, 'Joe Root', 'England');
+
+-- Insert into matches
+INSERT INTO matches VALUES
+(101, '2023-10-15', 'Ahmedabad'),
+(102, '2023-10-20', 'Melbourne');
+
+-- Insert into player_stats
+INSERT INTO player_stats VALUES
+(1001, 1, 101, 85, 0),
+(1002, 2, 101, 40, 0),
+(1003, 3, 102, 70, 0);
